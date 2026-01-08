ca36c1ba9c7d18c910b5391144f7e2c494c60db9
Rebeca Franco <159482574+rebecafrancog@users.noreply.github.com>
2026-01-08 20:13:49 -0300

    Atualizar o Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -25,9 +25,9 @@ export const Hero: React.FC<HeroProps> = ({ id }) => {
           </div>
           
           <h1 className="text-5xl md:text-7xl font-bold leading-tight">
-            Hi, I'm <br />
+            Olá. Eu sou a <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500">
-              Rebeca Franco Gomes
+              Rebeca Franco
             </span>
           </h1>
 
