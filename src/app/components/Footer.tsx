cd98cc60c6d0ff9aae5d9c5eaa12a5034230c05c
Rebeca Franco <159482574+rebecafrancog@users.noreply.github.com>
2026-01-08 20:11:28 -0300

    Atualizar o Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="py-8 border-t border-gray-900 bg-black mt-20">
       <div className="container mx-auto px-6 text-center text-gray-400">
         <p>© {new Date().getFullYear()} Rebeca Franco Gomes. All rights reserved.</p>
-        <p className="text-sm mt-2 text-gray-600">Built with React, CSS, HTML, JavaScript</p>
+        <p className="text-sm mt-2 text-gray-600">Built with React, Tailwind, Ionic</p>
       </div>
     </footer>
   );
